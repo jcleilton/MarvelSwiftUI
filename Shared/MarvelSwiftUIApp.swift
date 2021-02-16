935b691518e248b02b3f5371301a656bc89a1282
josefeitosa <josefeitosa@brq.com>
2021-02-16 13:58:07 -0300

    Initial Commit

---
+++ Shared/MarvelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelSwiftUIApp.swift
+//  Shared
+//
+//  Created by Jose Cleilton Feitosa on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
